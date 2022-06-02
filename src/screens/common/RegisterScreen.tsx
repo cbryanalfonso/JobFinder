b6e85a612871d0fdedfc28f4e545a1a79614c97f
cbryanalfonso <cbryanalfonso@gmail.com>
2022-06-02 12:09:50 -0500

    add Register Screen

---
+++ src/screens/common/RegisterScreen.tsx
@@ -0,0 +1,104 @@
+import React from 'react'
+import { SafeAreaView, StyleSheet, View } from 'react-native'
+import { ButtonUI } from '../../components/Buttons/Button';
+import { InputText } from '../../components/Input/InputText';
+import { TextUi } from '../../components/Text/TextUi';
+import { widthPercentageToDP as wp } from 'react-native-responsive-screen';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import { RootStackParamList } from '../../Hooks/navigations/exportNavigation';
+import { useNavigation } from '@react-navigation/core';
+type noAunth = NativeStackNavigationProp<RootStackParamList>;
+
+export const RegisterScreen = () => {
+    const navigation = useNavigation<noAunth>();
+  return (
+    <SafeAreaView style={styles.container}>
+    <View style={styles.headerContainer} >
+         <TextUi
+             style='txtTitleStart'
+             texto='Create an Account'
+             addStyle={{fontSize: wp(7)}}
+         />
+         <TextUi
+             style='txtTitleNormalCommon'
+             texto='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor'
+             addStyle={{marginTop: wp(2)}}
+         />
+    </View>
+    <View style={styles.bodyContainer}>
+        <TextUi
+         texto='Full Name'
+         style='txtCommonEtiquetas'
+
+        />
+    <InputText
+             placeholder='Full Name'
+        />
+         <TextUi
+         texto='Email'
+         style='txtCommonEtiquetas'
+         addStyle={{marginTop: wp(5)}}
+        />
+    <InputText
+             placeholder='Email'
+             autocapitalize={true}
+        />
+     <TextUi
+         texto='Password'
+         style='txtCommonEtiquetas'
+         addStyle={{marginTop: wp(5)}}
+        />
+         <InputText
+             placeholder='Password'
+             passwordShow={true}
+             autocapitalize={true}
+        />
+     
+        <ButtonUI
+         style={'buttonCommonLR'}
+         text='Sign Up'
+
+        />
+         <ButtonUI
+         style={'buttonCommonGoogle'}
+         text='SIGN UP WITH GOOGLE'
+         addStyle={{marginTop: wp(0)}}
+         image={true}
+
+        />
+
+        <View style={{flexDirection: 'row'}}>
+            <TextUi
+             style='txtNormal'
+             texto="You have an account? "
+            />
+            <TextUi
+             style='txtNormalOrange'
+             texto="Sign In "
+             onPress={()=>{navigation.navigate('LoginScreen')}}
+            />
+        </View>
+    </View>
+</SafeAreaView>
+  )
+}
+
+
+
+const styles = StyleSheet.create({
+    container:{
+        flex: 1,
+    },
+    headerContainer:{
+        flex:1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    bodyContainer:{
+        flex: 3,
+        alignItems: 'center'
+    },
+    containerFormik:{
+       flex: 1,
+    }
+});
