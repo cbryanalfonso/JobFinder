1c53ed7b7208c00fd50b9a9270acfcd0b187afd9
cbryanalfonso <cbryanalfonso@gmail.com>
2022-06-03 11:48:52 -0500

    add Button Profile

--- src/components/Buttons/ButtonProfile.tsx
+++ src/components/Buttons/ButtonProfile.tsx
@@ -1,23 +1,49 @@
-import { useNavigation } from '@react-navigation/core';
-import { NativeStackNavigationProp } from '@react-navigation/native-stack';
-import React from 'react'
-import { Image, SafeAreaView, TouchableOpacity } from 'react-native'
-import { widthPercentageToDP as wp } from 'react-native-responsive-screen';
-import { RootStackParamListAuth } from '../../Hooks/navigations/exportNavigation';
+import {useNavigation} from '@react-navigation/core';
+import {NativeStackNavigationProp} from '@react-navigation/native-stack';
+import React, { useState } from 'react';
+import {Image, SafeAreaView, TouchableOpacity} from 'react-native';
+import {widthPercentageToDP as wp} from 'react-native-responsive-screen';
+import {RootStackParamListAuth} from '../../Hooks/navigations/exportNavigation';
+import { firebase as auth } from '@react-native-firebase/auth';
 
-type navigationHeader = NativeStackNavigationProp<RootStackParamListAuth>
+type navigationHeader = NativeStackNavigationProp<RootStackParamListAuth>;
 
-export const ButtonProfile = () => {
-    const navigation = useNavigation<navigationHeader>();
-  return (
-    <TouchableOpacity
-        onPress={()=>{navigation.navigate('Profile')}}
-    >
-        <Image
-            source={require('../../assets/images/profile.png')}
-            style={{width: wp(10), height:wp(10)}}
-            resizeMode='contain'
-        />
-    </TouchableOpacity>
-  )
+interface Props {
+  edit?: boolean;
+  onPress?: () => void;
+  image?: string;
 }
+
+export const ButtonProfile = ({edit, onPress,image}: Props) => {
+  const navigation = useNavigation<navigationHeader>();
+
+  const [imagen, setImagen] = useState(auth.auth().currentUser?.photoURL);
+  return (
+    <>
+      {edit ? (
+        <TouchableOpacity
+        onPress={onPress}
+        style={{width: wp(20), alignItems: 'center'}}
+         >
+          <Image
+            source={imagen ? {uri: imagen} : require('../../assets/images/profile.png')}
+            style={{width: wp(20), height: wp(20), borderRadius: wp(8)}}
+            resizeMode="center"
+          />
+        </TouchableOpacity>
+      ) : (
+        <TouchableOpacity
+       // style={{width: wp(20), alignItems: 'center'}}
+          onPress={() => {
+            navigation.navigate('Profile');
+          }}>
+          <Image
+            source={imagen ? {uri: imagen} : require('../../assets/images/profile.png')}
+            style={{width: wp(10), height: wp(10), borderRadius: wp(8)}}
+            resizeMode="center"
+          />
+        </TouchableOpacity>
+      )}
+    </>
+  );
+};
