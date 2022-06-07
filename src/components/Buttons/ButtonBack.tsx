54a4cc4dbc124bb8112c54f734718bfc92092c6a
cbryanalfonso <cbryanalfonso@gmail.com>
2022-06-07 09:12:21 -0500

    modify styles button back

--- src/components/Buttons/ButtonBack.tsx
+++ src/components/Buttons/ButtonBack.tsx
@@ -8,27 +8,63 @@ import { useNavigation } from '@react-navigation/core';
 
 interface Props{
     onPress?: ()=>void;
+    style: string;
+    addStyle?: any;
+    colorIcon?: string;
 }
 
 type navigationHeader = NativeStackNavigationProp<RootStackParamListAuth>;
 
-export const ButtonBack = ({onPress}:Props) => {
+export const ButtonBack = ({onPress, style, addStyle,colorIcon}:Props) => {
     const navigation = useNavigation<navigationHeader>();
   return (
    <TouchableOpacity
     onPress={()=>navigation.goBack()}
-    style={{width: wp(10), height: wp(10), borderRadius: wp(5),
-    borderWidth: 1,
-    borderColor: '#FFF',
-    justifyContent: 'center',
-    alignItems: 'center'
-        }}
+    style={[getButtonStyle(style) as any,  addStyle ? addStyle : null]}
    >
        <Icon
             name='chevron-left'
             size={wp(8)}
-            color={'#FFF'}
+            color={colorIcon ? colorIcon : '#FFF'}
        />
    </TouchableOpacity>
   )
 }
+
+const getButtonStyle = (color: any) => {
+    switch (color) {
+      case 'buttonBackProfile':
+        return {
+            width: wp(10), 
+            height: wp(10), 
+            borderRadius: wp(5),
+            borderWidth: 1,
+            borderColor: '#FFF',
+            justifyContent: 'center',
+            alignItems: 'center'
+                };
+      case 'buttonBackAboutMe':
+        return {
+            width: wp(10), 
+            height: wp(10), 
+            borderRadius: wp(5),
+            justifyContent: 'center',
+            alignItems: 'center'
+        };
+      case 'buttonEditProfile':
+        return {
+          borderWidth: 1,
+          borderColor: '#FFF',
+          paddingVertical: wp(2),
+          paddingHorizontal: wp(2),
+          //marginVertical: wp(6),
+          borderRadius: wp(2),
+        };
+  
+      default:
+        return {
+          color: '#FFFFFF',
+          fontSize: 25,
+        };
+    }
+  };
