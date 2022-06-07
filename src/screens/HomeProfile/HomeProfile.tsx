e9754809d294ea47126f40b42971fe8eddfd2403
cbryanalfonso <cbryanalfonso@gmail.com>
2022-06-07 09:15:14 -0500

    add HomeJobs Screen

--- src/screens/HomeProfile/HomeProfile.tsx
+++ src/screens/HomeProfile/HomeProfile.tsx
@@ -1,68 +1,112 @@
 import {useNavigation} from '@react-navigation/core';
 import {NativeStackNavigationProp} from '@react-navigation/native-stack';
 import React from 'react';
-import {Image, Platform, SafeAreaView, ScrollView, StatusBar, StyleSheet, View} from 'react-native';
+import {
+  Image,
+  Platform,
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  View,
+} from 'react-native';
 import {widthPercentageToDP as wp} from 'react-native-responsive-screen';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
-import { ButtonUI } from '../../components/Buttons/Button';
+import {ButtonUI} from '../../components/Buttons/Button';
 import {ButtonProfile} from '../../components/Buttons/ButtonProfile';
 import {TextUi} from '../../components/Text/TextUi';
 import {RootStackParamListAuth} from '../../Hooks/navigations/exportNavigation';
-import { useProfile } from '../../Hooks/Profile/useProfile';
-import { ButtonBack } from '../../components/Buttons/ButtonBack';
-import { ButtonEditProfile } from '../../components/Buttons/ButtonEditProfile';
+import {useProfile} from '../../Hooks/Profile/useProfile';
+import {ButtonBack} from '../../components/Buttons/ButtonBack';
+import {ButtonEditProfile} from '../../components/Buttons/ButtonEditProfile';
+import {ButtonInformation} from '../../components/Buttons/ButtonInformation';
 
 type navigationHeader = NativeStackNavigationProp<RootStackParamListAuth>;
 
 export const HomeProfile = () => {
   const navigation = useNavigation<navigationHeader>();
 
-  const {
-    openCameraPhoto,
-    imagen,
-    name,
-  } = useProfile();
+  const {openCameraPhoto, 
+    imagen, 
+    name, 
+    showAboutMe, 
+    setShowAboutMe} =
+    useProfile();
 
   return (
     <>
-      <StatusBar hidden barStyle={'dark-content'} />
+      <StatusBar barStyle={'light-content'} backgroundColor={'#130160'} />
       <View style={styles.container}>
         <View style={styles.containerHeader}>
-          <View style={{flexDirection: 'row', justifyContent: 'space-between', marginHorizontal: wp(5), marginBottom: wp(4)}}>
-          <View style={{marginTop: Platform.OS === 'ios' ? wp(10) : wp(5)}} >
-            <ButtonBack   />
-          </View>
-          <View style={styles.topConf}>
-            <Icon name="share-outline" size={wp(8)} color="#FFF" />
-            <Icon name="cog-outline" size={wp(8)} color="#FFF" />
-          </View>
+          <View
+            style={{
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+              marginHorizontal: wp(5),
+              marginBottom: wp(4),
+            }}>
+            <View style={{marginTop: Platform.OS === 'ios' ? wp(10) : wp(5)}}>
+              <ButtonBack style="buttonBackProfile" />
+            </View>
+            <View style={styles.topConf}>
+              <Icon name="share-outline" size={wp(8)} color="#FFF" />
+              <Icon name="cog-outline" size={wp(8)} color="#FFF" />
+            </View>
           </View>
           <View style={{marginHorizontal: wp(5)}}>
-           <ButtonProfile edit={true} onPress={()=>openCameraPhoto()}  />
-            <TextUi texto={name ? name : 'Nombre Usuario'} style={'txtNormalProfileHeader'} />
+            <ButtonProfile edit={true} onPress={() => openCameraPhoto()} />
+            <TextUi
+              texto={name ? name : 'Nombre Usuario'}
+              style={'txtNormalProfileHeader'}
+            />
           </View>
-          <View style={{flexDirection: 'row', marginHorizontal: wp(5), marginTop: wp(2), justifyContent: 'space-between'}}>
-            <View style={{flexDirection: 'row'}}> 
+          <View
+            style={{
+              flexDirection: 'row',
+              marginHorizontal: wp(5),
+              marginTop: wp(2),
+              justifyContent: 'space-between',
+            }}>
+            <View style={{flexDirection: 'row'}}>
               <TextUi texto="120k " style={'txtNormalProfileHeaderBold'} />
               <TextUi texto="Followers" style={'txtNormalProfileHeader'} />
             </View>
-            <View style={{flexDirection: 'row'}}> 
-            <TextUi texto="20k " style={'txtNormalProfileHeaderBold'} />
+            <View style={{flexDirection: 'row'}}>
+              <TextUi texto="20k " style={'txtNormalProfileHeaderBold'} />
               <TextUi texto="Following" style={'txtNormalProfileHeader'} />
             </View>
-            <View style={{flexDirection: 'row', }}> 
-              <ButtonUI style='buttonEditProfile' text={'Edit Profile'} iconRight={true}/>
+            <View style={{flexDirection: 'row'}}>
+              <ButtonUI
+                style="buttonEditProfile"
+                text={'Edit Profile'}
+                iconRight={true}
+              />
             </View>
           </View>
         </View>
-        <ScrollView 
-        contentContainerStyle={styles.containerBody}
-        >
-          <ButtonEditProfile/>
-          <ButtonEditProfile/>
-          <ButtonEditProfile/>
-          <ButtonEditProfile/>
-
+        <ScrollView
+          contentContainerStyle={{alignItems: 'center', marginBottom: wp(10)}}>
+            {showAboutMe ? (
+              <ButtonInformation
+            iconName="account"
+            txt="About me"
+            txtInformation="EJEMPLO DE TEXT"
+            onPress={() => {
+              navigation.navigate('AboutMe');
+            }}
+          />
+            )
+            :( <ButtonEditProfile iconName="account" txt="About me" onPress={()=>{
+              setShowAboutMe(true)
+            }} />)}
+         
+          
+          <ButtonEditProfile iconName="account" txt="Work experience" />
+          <ButtonEditProfile iconName="account" txt="Education" />
+          <ButtonEditProfile iconName="account" txt="Skill" />
+          <ButtonEditProfile iconName="account" txt="Language" />
+          <ButtonEditProfile iconName="account" txt="Appreciation" />
+          <ButtonEditProfile iconName="account" txt="Resume" />
         </ScrollView>
       </View>
     </>
@@ -74,18 +118,22 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   containerHeader: {
-    flex: 0.5,
+    //flex: 1,
+    paddingBottom: wp(5),
     backgroundColor: '#130160',
+    marginBottom: wp(5),
   },
   containerBody: {
     flex: 5,
-    alignItems: 'center',
-   paddingTop: wp(5)
-
+    //height: wp(120),
+    // alignItems: 'center',
+    paddingTop: wp(5),
+    // backgroundColor: 'red',
+    //marginBottom: wp(10)
   },
   topConf: {
     flexDirection: 'row',
-    marginTop:  Platform.OS === 'ios' ? wp(10) : wp(5),
+    marginTop: Platform.OS === 'ios' ? wp(10) : wp(5),
     width: wp(25),
     alignSelf: 'flex-end',
     marginRight: wp(2),
